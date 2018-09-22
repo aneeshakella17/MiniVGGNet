eddec725f8f01e3df822ee353ac6eae8cbbf1ec8
mac <mac@macs-MacBook-Pro-2.local>
2018-09-21 20:53:56 -0700

    small change

--- minivggnet_cifar10.py
+++ minivggnet_cifar10.py
@@ -5,6 +5,7 @@ from sklearn.preprocessing import LabelBinarizer
 from sklearn.metrics import classification_report
 from keras.datasets import cifar10
 
+model = MiniVGGNet.build(width = 32, height = 32, depth = 3, classes = 10);
 
 print("[INFO] accessing CIFAR")
 (trainX, trainY), (testX, testY) = cifar10.load_data();
@@ -15,10 +16,14 @@ le.fit_transform(testY);
 
 print ("[INFO] Compiling model ...");
 opt = SGD(lr = 0.01);
-model = MiniVGGNet.build(width = 28, height = 28, depth = 1, classes = 10);
 
 model.compile(loss="categorical_crossentropy", optimizer=opt,
 	metrics=["accuracy"])
+
+print ("[INFO] BUILDING MODEL")
+H = model.fit(trainX, trainY, validation_data=(testX, testY),
+	batch_size=128, epochs=20, verbose=1)
+
 
 print("[INFO] evaluating network...")
 predictions = model.predict(testX, batch_size=128)
