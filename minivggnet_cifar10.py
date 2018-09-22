9c55c3dffb31462dcb64586210ec4878b1e08abe
mac <mac@macs-MacBook-Pro-2.local>
2018-09-21 21:19:47 -0700

    added checkpoint

--- minivggnet_cifar10.py
+++ minivggnet_cifar10.py
@@ -4,8 +4,10 @@ from keras.optimizers import SGD
 from sklearn.preprocessing import LabelBinarizer
 from sklearn.metrics import classification_report
 from keras.datasets import cifar10
+from keras.callbacks import ModelCheckpoint
 
 model = MiniVGGNet.build(width = 32, height = 32, depth = 3, classes = 10);
+weights = "artifacts/minivggnet.hdf5"
 
 print("[INFO] accessing CIFAR")
 (trainX, trainY), (testX, testY) = cifar10.load_data();
@@ -21,8 +23,11 @@ model.compile(loss="categorical_crossentropy", optimizer=opt,
 	metrics=["accuracy"])
 
 print ("[INFO] BUILDING MODEL")
+checkpoint = ModelCheckpoint(weights, monitor = "val_loss", save_best_only=True, verbose =1 );
+callbacks = [checkpoint];
+
 H = model.fit(trainX, trainY, validation_data=(testX, testY),
-	batch_size=128, epochs=20, verbose=1)
+	batch_size=128, epochs=90, callbacks = callbacks, verbose=1)
 
 
 print("[INFO] evaluating network...")
