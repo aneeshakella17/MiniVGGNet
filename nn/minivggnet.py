f53fb6f750de0f8f14e36c60f17c757553d5184a
mac <mac@macs-MacBook-Pro-2.local>
2018-09-21 20:49:42 -0700

    change

--- nn/minivggnet.py
+++ nn/minivggnet.py
@@ -18,21 +18,21 @@ class MiniVGGNet:
 
         model.add(Conv2D(32, (3,3), padding = "same", input_shape = inputShape));
         model.add(Activation("relu"));
-        model.add(BatchNormalization(chanDim=chanDim));
+        model.add(BatchNormalization(axis=chanDim));
 
         model.add(Conv2D(32, (3,3), padding = "same", input_shape = inputShape));
         model.add(Activation("relu"));
-        model.add(BatchNormalization(chanDim=chanDim));
+        model.add(BatchNormalization(axis=chanDim));
         model.add(MaxPooling2D(pool_size=(2,2)))
         model.add(Dropout(0.25))
 
         model.add(Conv2D(64, (3, 3), padding="same", input_shape=inputShape));
         model.add(Activation("relu"));
-        model.add(BatchNormalization(chanDim=chanDim));
+        model.add(BatchNormalization(axis=chanDim));
 
         model.add(Conv2D(64, (3, 3), padding="same", inputShape=inputShape));
         model.add(Activation("relu"));
-        model.add(BatchNormalization(chanDim=chanDim));
+        model.add(BatchNormalization(axis=chanDim));
         model.add(MaxPooling2D(pool_size=(2, 2)))
         model.add(Dropout(0.25))
 
